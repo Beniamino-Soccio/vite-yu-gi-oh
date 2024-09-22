de14a69be34b3177d45eb8ab32f288a84a9153f9
Beniamino-Soccio <beniamino43@gmail.com>
2024-09-22 18:47:39 +0200

    added loader

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,20 +1,21 @@
 <script>
 import MainCardList from './MainCardList.vue';
+import MainLoader from './MainLoader.vue';
 import axios from "axios";
 
 export default{
     data(){
         return{
-              cardsList : [],
-              apiUrl : "https://db.ygoprodeck.com/api/v7/cardinfo.php",
-
+            cardsList : [],
+            apiUrl : "https://db.ygoprodeck.com/api/v7/cardinfo.php",
+            loading : true,
         }
     },
     methods: {
         getCardsList(numOfCards){
             axios.get(this.apiUrl, {
                 params: {
-                    num : numOfCards,
+                    num : 40,
                     offset: 0,
                 }
             })
@@ -22,27 +23,31 @@ export default{
                     // handle success
                     console.log(response.data.data);
                     this.cardsList = response.data.data;
+
                 })
                 .catch(function (error) {
                     // handle error
                     console.log(error);
                 })
-                .finally(function () {
+                .finally(() => {
+                    this.loading = false;
                     // always executed
                 });
         }
     },
     created(){
-        this.getCardsList(40);
+        setTimeout(this.getCardsList, 2000);
     },
     components:{
         MainCardList,
+        MainLoader,
     }
 }
 </script>
 
 <template>
-    <MainCardList :cards="cardsList" />
+    <MainLoader v-if="loading"/>
+    <MainCardList v-else :cards="cardsList" />
 </template>
 
 <style lang="scss" scoped>
